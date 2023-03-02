b87d38c6d951d669862988e10a8184b03947050a
Lucas Salles <lucas.sarantes@gmail.com>
2023-03-01 22:07:23 -0300

    Construção inicial do primeiro passo do formulário

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,9 @@
-const colors = require('tailwindcss/colors');
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
-  darkMode: false, // or 'media' or 'class'
+  content: [],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-    },
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      black: '#464547',
-      white: colors.white,
-      gray: colors.trueGray,
-      indigo: colors.indigo,
-      red: colors.rose,
-      yellow: colors.amber,
-      orange: '#F9423A',
-      purple: colors.purple,
-      green: colors.green,
-    },
-    backgroundColor: (theme) => ({
-
-      ...theme('colors'),
-      primary: '#F9423A',
-      secondary: '#ffed4a',
-      danger: '#e3342f',
-      orange: '#F9423A',
-      blue: '#009BBF',
-    }),
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
-};
+}
